4fe184219c02bdef11fe29c8d42dbd03c91b8a10
Rafly09zr <rafly.zaki2902@mail.ugm.ac.id>
2023-11-29 18:17:35 +0700

    Update: contract and json

--- contracts/NotesList.sol
+++ contracts/NotesList.sol
@@ -3,6 +3,21 @@ pragma solidity ^0.5.0;
 contract NotesList {
  uint public notesCount = 0;
     
- 
+ struct Notes {
+    uint id;
+    string content;
+    bool completed;
+ }
+
+ mapping(uint => Notes) public notes;
+
+constructor()public {
+    createNotes("Please add some notes");
+}
+
+ function createNotes(string memory _content) public {
+    notesCount++;
+    notes[notesCount] = Notes(notesCount, _content, false);
+ }
 
 }
