b89f5aec4e3ed5ac0197db6dc0646cf611fd2009
Rafly09zr <rafly.zaki2902@mail.ugm.ac.id>
2023-11-30 16:21:26 +0700

    Update: adding new event and function in contract

--- contracts/NotesList.sol
+++ contracts/NotesList.sol
@@ -11,6 +11,17 @@ contract NotesList {
 
  mapping(uint => Notes) public notes;
 
+ event NotesCreated(
+   uint id,
+   string content,
+   bool completed
+ );
+
+  event NotesCompleted(
+   uint id,
+   bool completed
+  );
+
 constructor()public {
     createNotes("Please add some notes");
 }
@@ -18,6 +29,14 @@ constructor()public {
  function createNotes(string memory _content) public {
     notesCount++;
     notes[notesCount] = Notes(notesCount, _content, false);
+    emit NotesCreated(notesCount, _content, false);
+ }
+
+ function toggleCompleted(uint _id) public {
+    Notes memory _notes = notes[_id];
+    _notes.completed = !_notes.completed;
+    notes[_id] = _notes;
+    emit NotesCompleted(_id, _notes.completed);
  }
 
 }
